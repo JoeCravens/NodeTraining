2e070a0f49927499158e53684b914a72f60f11ac
Joe Cravens <joe.cravens@rigzone.com>
2016-12-09 12:12:54 -0600

    vC3

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-var http = require("http");
+var express = require("express");
+
+var app = express();
+
+app.get('/',function(req, res){
+	
+	res.send("hello Joe Joe");
+	
+	
+});
+app.get ('/users', function (reg,res){
+	res.send("Hello JOJO");
+	
+	
+});
+
+app.get('/users/:id', function (req,res){
+	
+	res.send("Hello User ID:"+req.params.id);
+	
+});
+
+app.listen(8081, function(){console.log("live on port 8081")});
+
+
+
+
+
+
+
+
+
+/*var http = require("http");
 var url = require("url");
 
 http.createServer(function (req,res){
-	var queryData = url.parse(req.url,true).query;
+	var queryData = url.parse(req.url);
+	
+	var pth = queryData.pathname.split("/");
+
+console.log(pth);
+	
 	res.writeHead(200,{'ContentType':'text/plain'});
 	
-	if (queryData.id)
-	{
-		res.end('Hello User:'+queryData.id+'\n');
-	}
-	else
-	{
+
 		res.end('Unknown Request\n');
 		
-	}
-	
-	res.end('Hello Joe');	
+
 }).listen(8081);
 
-console.log("Server Running!");
+console.log("Server Running!");*/
 
