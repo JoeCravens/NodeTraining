02bc0fb0452dc9a5b90f2bcf6dc2d9c6415f8d75
Joe Cravens <joe.cravens@rigzone.com>
2016-12-09 12:19:41 -0600

    vC3D4

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ var express = require("express");
 
 var app = express();
 
+
+app.get ('/users', function (reg,res){
+	res.status(200).send("Hello JOJO");
+	
+	
+});
+
+app.get('/users/:id', function (req,res){
+	
+	res.status(200).send("Hello User ID:"+req.params.id);
+	
+});
+
+app.use("*", function (req,res){
+	res.status(404).send("404: Resource not found");
+	
+});
+
+
+app.listen(8081, function(){console.log("live on port 8081")});
+
+
+/*var express = require("express");
+
+var app = express();
+
 app.get('/',function(req, res){
 	
 	res.send("hello Joe Joe");
@@ -21,7 +47,7 @@ app.get('/users/:id', function (req,res){
 });
 
 app.listen(8081, function(){console.log("live on port 8081")});
-
+*/
 
 
 
